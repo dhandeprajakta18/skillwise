8a86877f674e0e9196c5d5c16a94896932568e91
Prajakta Dhande <dhandeprajakta1232gmail.com>
2025-10-07 23:59:20 +0530

    WIP: save local changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    domains: [
-      "via.placeholder.com",
-      "images.unsplash.com",
-      "picsum.photos"
-    ],
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
 };
-
-module.exports = nextConfig;
+export default nextConfig;
